8403c3d78f24a039f5e0d9d4388aecca651e2f91
ShauryaGarg9126 <71919446+ShauryaGarg9126@users.noreply.github.com>
2020-10-18 13:35:00 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,46 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+			
+			
+			}
+			
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		
+		
+		this.image = loadImage("paper.png");
+		World.add(world, this.body);
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+			
+			push()
+			translate(paperpos.x, paperpos.y);
+			
+			fill("lightgrey");
+			
+			rectMode(CENTER)
+			stroke("lightgrey");
+			ellipse(-30,-50,this.r);
+			
+			fill("lightgrey");
+			stroke("lightgrey")
+			strokeWeight(3);
+			imageMode(CENTER);
+			image(this.image,-35, -35, 90,90);
+			pop()
+			
+	}
+
+}
